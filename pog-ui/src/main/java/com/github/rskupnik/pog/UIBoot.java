d5dfc75e0c54187edbe944686a04be491a615e8a
Radosław Skupnik <r.skupnik@gmail.com>
2020-10-23 21:47:05 +0200

    Setup a basic multi-project build

---
+++ pog-ui/src/main/java/com/github/rskupnik/pog/UIBoot.java
@@ -0,0 +1,10 @@
+package com.github.rskupnik.pog;
+
+import com.github.rskupnik.pog.ui.PogApplication;
+
+public class UIBoot {
+
+    public static void boot() {
+        PogApplication.launchUI();
+    }
+}
---
+++ src/main/java/com/github/rskupnik/pog/PathOfGuiding.java
@@ -0,0 +1,14 @@
+package com.github.rskupnik.pog;
+
+import java.nio.file.Path;
+
+public class PathOfGuiding {
+
+    private void start() {
+        UIBoot.boot();
+    }
+
+    public static void main(String[] args) {
+        new PathOfGuiding().start();
+    }
+}
